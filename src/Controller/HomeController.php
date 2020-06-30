96ccc5a8f52474330237f97f48d3c3680d70644c
Erik <erik.silva.fuentes@gmail.com>
2020-06-30 18:06:57 -0400

    agregado creditos empresas factoring garantias y leasing

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -22,5 +22,33 @@ class HomeController extends AbstractController
     {
         return $this->render('home/factoring.html.twig');
     }
+       /**
+     * @Route("/leasing", name="leasing")
+     */
+    public function leasing()
+    {
+        return $this->render('home/leasing.html.twig');
+    }
+       /**
+     * @Route("/creditos", name="creditos")
+     */
+    public function creditos()
+    {
+        return $this->render('home/creditos.html.twig');
+    }
+        /**
+     * @Route("/garantias", name="garantias")
+     */
+    public function garantias()
+    {
+        return $this->render('home/garantias.html.twig');
+    }
+            /**
+     * @Route("/empresa", name="empresa")
+     */
+    public function empresa()
+    {
+        return $this->render('home/empresa.html.twig');
+    }
 
 }

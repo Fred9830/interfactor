3a992e6c18502359e2a673bef01365f7d195ce5a
Erik <erik.silva.fuentes@gmail.com>
2020-07-07 18:22:06 -0400

    estructura single post faq y ejecutivos

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -71,6 +71,21 @@ class HomeController extends AbstractController
     {
         return $this->render('home/single_blog.html.twig');
     }
+                  /**
+     * @Route("/faq", name="faq")
+     */
+    public function faq()
+    {
+        return $this->render('home/faq.html.twig');
+    }
+
+                      /**
+     * @Route("/ejecutivos", name="ejecutivos")
+     */
+    public function ejecutivos()
+    {
+        return $this->render('home/ejecutivos.html.twig');
+    }
 
 
 }

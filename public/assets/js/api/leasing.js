84bf24f6593f667a5085465209b030d79693bc1f
josemanuel <josemaflorez1823@gmail.com>
2020-08-04 18:08:54 -0400

    merge pull josema

--- public/assets/js/api/leasing.js
+++ public/assets/js/api/leasing.js
@@ -2,46 +2,34 @@ jQuery(document).ready(function() {
     
     jQuery('#enviarleasing').on("click", function(e) {
 
-
-        /*
-        window.ParsleyValidator.addValidator('typeFiles', {
-            validateString: function(_value, maxSize, parsleyInstance) {
-            
-            var files = parsleyInstance.$element[0].files;
-            var image = files[0];
-            var result = (/\.(jpg|jpeg|png)$/i).test(image.name);
-            return result === true;
-            },
-            messages: {
-            en: 'El archivo debe ser PNG o JPG',
-            }
-        });
-
-        window.Parsley.addValidator('maxFileSize', {
-            validateString: function(_value, maxSize, parsleyInstance) {
-            
-            var files = parsleyInstance.$element[0].files;
-            return files.length != 1  || files[0].size <= maxSize * 1024;
-            },
-            requirementType: 'integer',
-            messages: {
-            en: 'Este archivo no debe ser mayor que %s Kb '
-            }
-        });
-        */
-
-
         var form = jQuery('#form-leasing');
+        dato = jQuery('#form-leasing').serialize();
         form.parsley().validate();
         
         if (form.parsley().isValid()){
-            //product.setData(TableProduct);
 
-            console.log('formulario validado'); 
-
+         console.log(dato); 
         }
         
         e.preventDefault();
+
+        /*
+        jQuery.ajax({
+            url: '/save_leasing',
+            type: 'POST',
+            contentType: false,
+            data: datas,
+            processData: false,
+            cache: false,
+            success: function(resp) {
+                if (resp) {
+
+                    console.log(resp); 
+        
+                }
+            }
+        });*/
+
         
 
     });
--- src/Controller/LeasingController.php
+++ src/Controller/LeasingController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Leasing;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class LeasingController extends AbstractController
 {
@@ -14,4 +17,40 @@ class LeasingController extends AbstractController
     {
         return $this->render('home/leasing.html.twig');
     }
+
+
+     /**
+     * @Route("/save_leasing", methods={"POST"})
+     */
+    public function save_leasing (Request $request)
+    {
+
+    
+        $params =$request->request->all();
+        $em = $this->getDoctrine()->getManager();
+        
+        dump($params);
+        exit;
+
+
+
+        $leasing = new Leasing();
+        $leasing->setName($params['nombreempresa']);
+        $leasing->setRut($params['rutempresa']);
+        $leasing->setPhone($params['telefono']);
+        $leasing->setEmail($params['email']);
+        $leasing->setMessage($params['mensaje']);
+
+        $em->persist($leasing);
+        $em->flush();
+
+        $data =[
+            'status' => 'success',
+            'code'   => 200,
+            'message' => 'Procesado exitosamente'
+        ];
+
+        return $this->resjson($data);
+    }
+
 }
